86077f63169002ce9b50aedd5d693e002a571bbb
Dawit <dawit@ginetta.net>
2020-04-03 19:51:24 +0200

    Add an array of objects to be requested

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ const app = http.createServer((req, res) => {
 
 const port = 3001;
 
+const personalPhoneEntry = [
+	{
+		name: 'Arto Hellas',
+		number: '040-123456',
+		id: 1
+	},
+	{
+		name: 'Arto Hellas',
+		number: '39-44-5323523',
+		id: 4
+	},
+	{
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+		id: 3
+	},
+	{
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+		id: 4
+	}
+];
+app.get('/api/personalPhoneEntry', (req, res) => {
+	res.json(personalPhoneEntry);
+});
 app.listen(port);
 
 console.log(`Server running on port ${port}`);
